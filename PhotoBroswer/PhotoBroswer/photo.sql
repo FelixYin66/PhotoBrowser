3dee8bea58bab149f458c611513f21ff72c402a0
felixyiphone <yinxianjin66@163.com>
2015-08-18 15:24:26 +0800

    修改之前的bug,上拉，下拉加载数据

--- PhotoBroswer/PhotoBroswer/photo.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS "T_Photo" (
-"photoid" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-"smallphotourl" TEXT NOT NULL,
-"largephotourl" TEXT NOT NULL
-);
--- PhotoBroswer/Pods/Target Support Files/Pods-PhotoBroswer-SDWebImage/Pods-PhotoBroswer-SDWebImage-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PhotoBroswer_SDWebImage : NSObject
-@end
-@implementation PodsDummy_Pods_PhotoBroswer_SDWebImage
-@end
